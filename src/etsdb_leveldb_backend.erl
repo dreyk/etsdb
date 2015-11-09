cfc90ea4fc434ee6e9b1acde922f2d58880cdcfc
Dreyk <gunin@satissoft.ru>
2015-11-09 03:40:26 +0300

    Repair db

--- src/etsdb_leveldb_backend.erl
+++ src/etsdb_leveldb_backend.erl
@@ -384,7 +384,9 @@ open_db(State0, RetriesLeft, _) ->
                     {error, Reason}
             end;
         {error, Reason} ->
-            {error, Reason}
+            lager:info("Try repair DB ~p",[Reason]),
+            eleveldb:repair(State0#state.data_root,State0#state.open_opts),
+            open_db(State0, RetriesLeft - 1, Reason)
     end.
 
 %% @private
